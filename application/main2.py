b1d7506f50b183c7fc19c4fdae80991761020564
remy 666 <remy.deplanque@laposte.net>
2023-02-17 15:54:24 +0100

    df

--- application/main2.py
+++ application/main2.py
@@ -20,7 +20,7 @@ vars["str_titre"] = driver.title
 assert(vars["str_titre"] == "vars[input_title]")
 
 print("{}".format(vars["input_title"]))
-print("Bonjour je suis un messa")
+print("Bonjour je suis un")
 
 
 
